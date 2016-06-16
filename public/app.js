819623f4017983d7b73e6cb34afcbcb8669bb4b6
Yini Shi <yini.shi@gmail.com>
2016-06-16 16:12:11 -0400

    did some layout stuff

--- public/app.js
+++ public/app.js
@@ -1,30 +1,21 @@
-var app = angular.module('botRoulette', ['ui.router']);
-
-// app.controller('ChatCtrl', function($scope) {
-
-// })
-
-// app.factory('ChatFact', function($scope) {
-	
-// })
-
 var socket = io();
+
 $('form').submit(function() {
-	msg = $('#m').val()
-    socket.emit('chat message', msg);
-    $('#messages').append($('<li>').text("you: " + msg));
-    $('#m').val('');
-    return false;
+  msg = $('#m').val();
+  socket.emit('chat message', msg);
+  $('#messages').append($('<li>').text("you: " + msg));
+  $('#m').val('');
+  return false;
 });
 
 socket.on('match status', function(msg) {
-    $('#messages').append($('<li class="automated-msg">').text(msg));
+  $('#messages').append($('<li class="automated-msg">').text(msg));
 });
 
 socket.on('reply', function(response) {
-    $('#messages').append($('<li>').text("partner: " + response));
+  $('#messages').append($('<li>').text("partner: " + response));
 });
 
 socket.on('partner left', function(response) {
-    $('#messages').append($('<li class="automated-msg">').text(response));
+  $('#messages').append($('<li class="automated-msg">').text(response));
 });
