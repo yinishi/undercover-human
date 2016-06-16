4363acbf09cecc4deb5515e563484dd4cba4c5d9
Yini Shi <yini.shi@gmail.com>
2016-06-16 13:09:42 -0400

    working partner match and client-to-client chat

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
 var socket = io();
 $('form').submit(function() {
-    socket.emit('chat message', $('#m').val());
-    $('#messages').append($('<li>').text("user1: " + $('#m').val()));
+	msg = $('#m').val()
+    socket.emit('chat message', msg);
+    $('#messages').append($('<li>').text("you: " + msg));
     $('#m').val('');
     return false;
 });
-socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text("user2: " + msg));
+
+socket.on('reply', function(response) {
+    $('#messages').append($('<li>').text("partner: " + response));
 });
--- server/start.js
+++ server/start.js
@@ -1,4 +1,3 @@
-
 var path = require('path');
 var bodyParser = require('body-parser')
 
@@ -21,8 +20,8 @@ var bot = Bot.bot;
 bot.loadDirectory(__dirname + '/bot/brain', Bot.success_handler, Bot.error_handler);
 
 /* START SERVER */
-server.listen(1337, function () {
-    console.log('The server is listening on port 1337!');
+server.listen(1337, function() {
+  console.log('The server is listening on port 1337!');
 });
 
 
@@ -31,31 +30,76 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/bot', router)
+app.use('/bot', router);
 
 // Routes
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-// app.get('/:room', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
 
 // Sockets start here
 
+var unmatched = [];
+
+function coinFlip() {
+  return Math.floor(Math.random() * 2 + 1);
+}
+
+function findPartner(mySocket) {
+
+  // 50% chance of getting matched with a bot
+  if (coinFlip() % 2) {
+    return {id: 'bot'};
+
+    // there are unmatched people
+  } else if (unmatched.length > 0) {
+
+    // make sure you don't get paired with yourself
+    if (unmatched[0].id === mySocket.id) {
+      return unmatched.splice(1, 1);
+    } else {
+      var partner = unmatched.shift();
+      partner.partner = mySocket;
+      return partner;
+    }
+
+    // there are no unmatched people, get added to the queue and wait
+  } else if (unmatched.length < 1) {
+    unmatched.push(mySocket);
+    console.log('waiting for a partner');
+  }
+}
+
+
 io.on('connection', function(socket) {
+  console.log('-----------------');
   console.log(socket.id, 'connected');
+  console.log('unmatched users after joining', unmatched.map(person => person.id))
+
+  socket.partner = findPartner(socket)
+  var partner = socket.partner || 'n/a'
+  console.log('found a partner for', socket.id, ':', partner.id);
+  console.log('unmatched users after match', unmatched.map(person => person.id));
 
   socket.on('chat message', function(msg) {
-    setTimeout(function() {
-      io.emit('chat message', bot.reply("local-user", msg));
-    }, Math.random() * 6000)
-    
+
+    // if your partner is a bot...
+    if (socket.partner.id === 'bot') {
+      console.log('chatting with bot', socket.id)
+      // io.to() emits the response to the socket that sent the message only
+      io.to(socket.id).emit('reply', bot.reply(socket.id, msg));
+
+    // if your partner is human...
+    } else {
+      io.to(socket.partner.id).emit('reply', msg);
+    }
+
   })
 
   socket.on('disconnect', function() {
+    unmatched.splice(unmatched.indexOf(socket.id), 1)
+    console.log('-----------------------');
+    console.log('after ', socket.id, 'left:', unmatched)
     console.log(socket.id, 'disconnected from the server');
   });
 
